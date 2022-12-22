7b394d2bf3517b0ba79ea56d2c2b859db1faa8ed
Dzaky Zakiyal Fawwaz <dzakybd@gmail.com>
2022-12-22 10:00:41 +0900

    qtrickle

--- 6tisch-simulator/SimEngine/Mote/trickle_qtrickle.py
+++ 6tisch-simulator/SimEngine/Mote/trickle_qtrickle.py
@@ -31,7 +31,6 @@ class QTrickle(object):
         # shorthand to singletons
         self.engine   = SimEngine.SimEngine.SimEngine()
         self.settings = SimEngine.SimSettings.SimSettings()
-        self.log      = SimEngine.SimLog.SimLog().log
 
         # constants of this timer instance
         # min_interval is expected to given in milliseconds
@@ -47,8 +46,6 @@ class QTrickle(object):
         self.interval = 0
         self.user_callback = callback
         self.state = self.STATE_STOPPED
-
-        # =======================
         self.start_t_record = None
         self.end_t_record = None
         self.ncells = None
@@ -59,24 +56,21 @@ class QTrickle(object):
         self.epsilon = self.settings.ql_epsilon
 
         self.i_max = i_max
-        self.m = -1
-        self.q_table = np.zeros([self.i_max, 2])
-        self.psuccess = np.zeros(self.i_max)
-        self.pcollision = np.zeros(self.i_max)
-        self.Ncell = np.zeros(self.i_max)
-        self.num_collision_busy = np.zeros(self.i_max)
-        self.prev_psuccess = 1
+        self.m = 0
+        self.q_table = np.zeros([self.i_max + 1, 2])
+        self.pfree = 1
+        self.poccupancy = 0
+        self.Ncells = 0
 
         self.Nreset = 0
         self.DIOtransmit = 0
         self.DIOtransmit_collision = 0
-        self.DIOtransmit_collision_prob = 0
-        self.Nstates = 0
+        self.Nstates = 1
         self.preset = 0
-        self.pstable = 0
-        self.Nnbr = 0
-        self.kmax = 10
-        self.ptransmit = 0
+        self.pstable = 1
+        self.kmax = self.settings.k_max
+        self.Nnbr = self.kmax
+        self.ptransmit = 1
         self.current_action = -1
         self.ptransmit_collision = 0
         self.t_start = 0
@@ -93,6 +87,8 @@ class QTrickle(object):
         #       Imin and less than or equal to Imax.  The algorithm then begins
         #       the first interval.
         self.state = self.STATE_RUNNING
+        # ok
+        self.m = 0
         self.interval = self.min_interval
         self._start_next_interval()
 
@@ -104,6 +100,7 @@ class QTrickle(object):
         self.state = self.STATE_STOPPED
 
     def reset(self, note=None):
+        # print(self.mote.id, f'reset {note}')
         if self.state == self.STATE_STOPPED:
             return
 
@@ -116,11 +113,14 @@ class QTrickle(object):
         #       step 2.  If I is equal to Imin when Trickle hears an
         #       "inconsistent" transmission, Trickle does nothing.  Trickle can
         #       also reset its timer in response to external "events".
+        
+        # ok
+        self.m = 0
+        self.Nreset += 1
+        self.calculate_reset_stable_prob()
+        
         if self.min_interval < self.interval:
             self.interval = self.min_interval
-            self.m = -1
-            self.Nreset += 1
-            self.calculate_reset_stable_prob()
             self._start_next_interval()
         else:
             # if the interval is equal to the minimum value, do nothing
@@ -135,10 +135,12 @@ class QTrickle(object):
         #       increments the counter c.
         self.counter += 1
 
+    # ok
     def calculate_reset_stable_prob(self):
         self.preset = self.Nreset / self.Nstates
         self.pstable = 1 - self.preset
     
+    # ok
     def calculate_k(self):
         self.Nnbr = self.kmax
         if hasattr(self.mote.rpl.of, 'neighbors'):
@@ -146,30 +148,6 @@ class QTrickle(object):
 
         k = 1 + math.ceil( min(self.Nnbr, self.kmax - 1) * self.preset)
         return k
-
-    def increment_dio_collision(self):
-        self.DIOtransmit_collision += 1
-        
-    def calculate_dio_transmit_collision(self):
-        self.DIOtransmit_collision_prob = self.DIOtransmit_collision / self.DIOtransmit
-
-    def calculate_ptransmit(self):
-        self.ptransmit = self.DIOtransmit / self.Nstates
-        self.ptransmit_collision = self.DIOtransmit_collision / self.DIOtransmit
-
-    def calculate_current_collision_success(self):
-        candidate_cells = self.mote.tsch.slotframes[0].get_cells_at_asn(1)
-        cell, packet = self.mote.tsch._select_active_cell(candidate_cells)
-
-        slotframe_len = self.settings.tsch_slotframeLength * self.settings.tsch_slotDuration * 1000 # convert to ms
-        self.Ncell[self.m] = int(math.ceil(old_div((self.t_end-self.t_start), slotframe_len)))
-
-        self.num_collision_busy[self.m] = 0
-
-        assert self.num_collision_busy[self.m] <= self.Ncell[self.m]
-        self.pcollision[self.m] = self.num_collision_busy[self.m] / self.Ncell[self.m]
-        self.psuccess[self.m] =  1 - self.pcollision[self.m]
-
 
     def _start_next_interval(self):
         if self.state == self.STATE_STOPPED:
@@ -191,9 +169,9 @@ class QTrickle(object):
         #       The interval ends at I.
         slot_len = self.settings.tsch_slotDuration * 1000 # convert to ms
 
-        # # Calculate T
+        # Calculate T
         half_interval = old_div(self.interval, 2)
-        self.t_start = half_interval * (self.ptransmit * self.prev_psuccess)
+        self.t_start = half_interval * (self.ptransmit * self.pfree)
         self.t_end = half_interval + (self.pstable * half_interval)
         t_range = self.t_end - self.t_start
         t = random.uniform(self.t_start, self.t_end)
@@ -211,16 +189,19 @@ class QTrickle(object):
             # the current ASN
             asn = self.engine.getAsn() + 1
 
-        def _transmit():
-            self.calculate_ptransmit()
-            self.current_action = 1
-            self.DIOtransmit += 1
-            self.is_dio_sent = True
+        if asn_start == self.engine.getAsn():
+            # schedule the event at the next ASN since we cannot schedule it at
+            # the current ASN
+            asn_start = self.engine.getAsn() + 1
+
+        if asn_end == self.engine.getAsn():
+            # schedule the event at the next ASN since we cannot schedule it at
+            # the current ASN
+            asn_end = self.engine.getAsn() + 1
 
         def _callback():
             self.mote.tsch.set_is_dio_sent(False)
             self.is_dio_sent = False
-
             if random.uniform(0, 1) <= self.epsilon:
                 # Explore action space
                 if self.counter < self.redundancy_constant:
@@ -228,11 +209,11 @@ class QTrickle(object):
                     #    4.  At time t, Trickle transmits if and only if the
                     #        counter c is less than the redundancy constant k.
                     self.user_callback()
-                    _transmit()
+                    self.is_dio_sent = True
                 else:
                     # do nothing
                     # print('do nothing')
-                    self.current_action = 0
+                    pass
 
             else:
                 # Exploit learned values
@@ -240,21 +221,11 @@ class QTrickle(object):
 
                 if action == 1:
                     self.user_callback()
-                    _transmit()
+                    self.is_dio_sent = True
                 else:
                     # do nothing
                     # print('do nothing')
-                    self.current_action = 0
-
-        # log
-        self.log(
-            SimEngine.SimLog.LOG_DEBUG_ASN,
-            {
-                u'_mote_id':        self.mote.id,
-                u'position':        2,
-                u'asn':             asn,
-            }
-        )
+                    pass
         
         def start_t():
             minimal_cell = self.mote.tsch.get_cell(0, 0, None, 0)
@@ -287,38 +258,53 @@ class QTrickle(object):
             intraSlotOrder = d.INTRASLOTORDER_STACKTASKS)
 
     def update_qtable(self):
-        reward = self.psuccess[self.m]
+        reward = self.pfree
+
         # clip m
         if self.i_max < (self.m + 1):
             next_m = self.m
         else:
             next_m = self.m + 1
 
-        next_max = np.max(self.q_table[next_m])
+        next_action = np.max(self.q_table[next_m])
         old_value = self.q_table[self.m][self.current_action]
-        td_learning = (reward + self.betha * next_max) - old_value
+        td_learning = (reward + self.betha * next_action) - old_value
 
         new_value = (1 - self.alpha) * old_value + self.alpha * td_learning
         self.q_table[self.m][self.current_action] = new_value
-        
-        self.prev_psuccess = self.psuccess[self.m]
 
     def _schedule_event_at_end_of_interval(self):
         slot_len = self.settings.tsch_slotDuration * 1000 # convert to ms
         asn = self.engine.getAsn() + int(math.ceil(old_div(self.interval, slot_len)))
 
-        self.update_qtable()
         def _callback():
             used = None
             occ = None
             if self.end_t_record is not None and self.start_t_record is not None:
                 dio_sent = int(self.mote.tsch.is_dio_sent) * int(self.is_dio_sent)
                 used = max((self.end_t_record - self.start_t_record) - dio_sent, 0)
-                occ = round(used/self.ncells, 2)
+                occ = round(used/self.ncells, 2)            
+                self.poccupancy = occ
+                self.pfree = 1 - self.poccupancy
+
+            # ok
+            self.current_action = 0
+            if self.is_dio_sent:
+                self.current_action = 1
+                self.DIOtransmit += 1
+                if not self.mote.tsch.is_dio_sent:
+                    self.DIOtransmit_collision += 1
+
+            # ok
+            self.ptransmit = self.DIOtransmit / self.Nstates
+            self.ptransmit_collision = (self.DIOtransmit_collision / self.DIOtransmit) if self.DIOtransmit else 0
 
             # if self.counter > 0:
             #     print("E", self.mote.id, self.end_t_record, used, occ, self.counter, self.redundancy_constant)
 
+            self.update_qtable()
+
+            # ok
             self.mote.tsch.set_is_dio_sent(False)
             self.is_dio_sent = False
             # doubling the interval
@@ -335,7 +321,6 @@ class QTrickle(object):
                 self.interval = self.max_interval
                 self.m = self.i_max
             self._start_next_interval()
-            self.calculate_current_collision_success()
 
         self.engine.scheduleAtAsn(
             asn            = asn,
